7aa5114aed11a19bb30fd8ada313ce40a995ee90
Taliraz <s.armangau@laposte.net>
2019-11-23 17:38:15 +0100

    correctifs

--- src/app/burger-form/name.validator.ts
+++ src/app/burger-form/name.validator.ts
@@ -2,6 +2,6 @@ import {AbstractControl, ValidatorFn} from "@angular/forms";
 
 export function nameContainsBurgerValidator(fix : string): ValidatorFn {
   return (control: AbstractControl): { [key: string]: any }  | null => {
-    return control.value.includes(fix) ? null : {'mustContain' : fix}
+    return control.value.toLowerCase().includes(fix) ? null : {'mustContain' : fix}
   }
 }
